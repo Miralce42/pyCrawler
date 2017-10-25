f6af78b09ca75331589e4b8db27d15f871987546
Miralce42 <291176820@qq.com>
2017-10-25 23:49:37 +0800

    Finanal

--- pretty_short_hair_girls.py
+++ pretty_short_hair_girls.py
@@ -12,18 +12,21 @@ from bs4 import BeautifulSoup
 
 
 def get_content(url, data=None):
-    header = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9',
-        'Connection': 'keep-alive',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3236.0 Safari/537.36'
-    }
-    timeout = random.choice(range(80, 180))
+    browser = webdriver.Firefox()
     while True:
         try:
-            rep = requests.get(url, headers=header, timeout=timeout)
-            rep.encoding = 'utf-8'
+            browser.get(url)
+            btn = browser.find_elements_by_class_name('QuestionMainAction')
+            cnt = 10
+            print('读取中...')
+            while cnt != 0 and len(btn) != 0:
+                cnt -= 1
+                print('.')
+                btn[0].click()
+                time.sleep(3)
+                btn = browser.find_elements_by_class_name('QuestionMainAction')
+            time.sleep(10)
+            pageSource = browser.page_source
             break
         except socket.timeout as e:
             print('3:', e)
@@ -41,7 +44,7 @@ def get_content(url, data=None):
             print('6:', e)
             time.sleep(random.choice(range(5, 15)))
 
-    return rep.text
+    return pageSource
 
 
 def store_pic(html_text):
@@ -50,13 +53,17 @@ def store_pic(html_text):
 
     path = os.path.abspath('.')  # 构造路径
     path = os.path.join(path, 'pretty_short_hair_girls')
-    os.mkdir(path)  # 创建根文件夹
-
+    #os.mkdir(path)  # 创建根文件夹
+    num = 0
     for girl in girls:
-        girl_a = girl.find_all('a', {'class': 'UserLink-link'})[1]
+        user = girl.find_all('a', {'class': 'UserLink-link'})
+        if len(user) == 0:
+            continue
+        girl_a = user[1]
         girl_name = girl_a.get_text()  # 获取用户名
         girl_url = 'www.zhihu.com' + girl_a['href']  # 获取用户链接
-        girl_path = path + '\\' + girl_name  # 构造用户链接
+        girl_path = path + '\\%s_' % num + girl_name  # 构造用户链接
+        print('爬取小姐姐 %s 的照片中...\n' % girl_name)
         os.mkdir(girl_path)  # 创建用户文件夹
         with open(girl_path + '\\inf.txt', 'w') as f:
             f.write(girl_name + '\n' + girl_url)
@@ -70,7 +77,7 @@ def store_pic(html_text):
             with open(girl_path + r'\%s.jpg' % cnt, 'wb') as fp:
                 fp.write(req.content)
             cnt += 1
-
+        num += 1
 
 if __name__ == '__main__':
     url = 'https://www.zhihu.com/question/40273344'
